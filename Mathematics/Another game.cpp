02748d9efaf0ade7e833d3781b05c4822af5e66f
Rupesh-110805 <rupeshnidadavolu110805@gmail.com>
2025-12-06 00:55:09 +0530

    Another Game

---
+++ Mathematics/Another game.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    for(int i = 0; i < n; i++){
+        if(a[i] & 1){
+            cout << "first\n";
+            return;
+        }
+    }
+    cout << "second\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
